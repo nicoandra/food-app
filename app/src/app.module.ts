dd802d0a972ba7222e43f3f8cad87302aa4fd5a1
Nico Andrade <nico@nmac.com.ar>
2020-12-05 19:40:44 -0500

    users

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 const mongoSettings = `mongodb://${process.env.MONGODB_HOST}/${process.env.MONGODB_DB}`
 const mongooseModule = MongooseModule.forRoot(mongoSettings, {connectionName: "default"})
 
 @Module({
-  imports: [mongooseModule],
+  imports: [mongooseModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
